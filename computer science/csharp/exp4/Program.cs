fd3b492dff07a994ea231b7cada0b1052a4355e8
h1542462994 <t1542462994@outlook.com>
2020-10-28 19:27:16 +0800

    CSharp：更新第5次实验

---
+++ computer science/csharp/exp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exp4
+{
+    class TimeSpan
+    {
+        private uint totalSeconds;    
+        
+        public TimeSpan(uint initialTotalSeconds)
+        {
+            totalSeconds = initialTotalSeconds;
+        }
+        
+        public void PrintSeconds()
+        {
+            Console.WriteLine("Total seconds: {0}", totalSeconds);
+        }
+        
+        public static implicit operator TimeSpan(uint convertFrom)
+        {
+            TimeSpan newTimeSpan;
+            
+            newTimeSpan = new TimeSpan(convertFrom);
+            Console.WriteLine("Converting from uint to TimeSpan");
+            return newTimeSpan;
+        }
+    }
+
+    class ImplicitConversionTester
+    {
+        public static void Main()
+        {
+            uint simpleTimeSeconds = 30;
+            TimeSpan myTime;
+            
+            myTime = simpleTimeSeconds;
+            myTime.PrintSeconds();
+        }
+    }
+
+}
